0dd5c4e072dcfabaf3d8e7a14eba3273856243cd
Remi Mendoza <mendozaremi@gmail.com>
2020-08-13 16:08:36 -0400

    created component Row firebase setup

---
+++ src/Row.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Row({title }) {
+  return (
+    <div>
+      {/*Title */}
+      <h2>{title}</h2>
+
+      {/* container -> posters */}
+
+    </div>
+  )
+}
+
+export default Row
---
+++ src/requests.js
@@ -0,0 +1,6 @@
+const APIKEY = "74516cab96a2824bc7dc5e7dabde44f4";
+
+const request = {
+
+
+}

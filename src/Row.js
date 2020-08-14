4e9a8b282326f8202c2de4e5a5d55da876c222cf
Remi Mendoza <mendozaremi@gmail.com>
2020-08-13 20:14:38 -0400

    finished rows working on banner

--- src/Row.js
+++ src/Row.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
+import './Row.css';
 
-function Row({title }) {
+const base_url = 'https://image.tmdb.org/t/p/original/';
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+
+  // A snippet of code which runs based on a specefic condition
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+    //if [] , run once and dont run again
+  }, [fetchUrl]);
+
   return (
-    <div>
-      {/*Title */}
+    <div className="row">
       <h2>{title}</h2>
 
-      {/* container -> posters */}
-
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row_poster ${isLargeRow && 'row_posterLarge'}`}
+            src={`${base_url}${
+              isLargeRow ? movies.poster_path : movies.backdrop_path
+            }`}
+            alt={movies.name}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Row
+export default Row;
